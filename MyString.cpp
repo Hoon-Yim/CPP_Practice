d08d41f20129641a7727dc9d8c4f813bf7341b3c
Hoon Yim <yimsh4507@gmail.com>
2024-03-14 11:20:00 -0400

    .gitignore updated

--- MyString.cpp
+++ MyString.cpp
@@ -7,11 +7,13 @@ MyString::MyString(char c)
     mStringContent = new char[1];
     mStringContent[0] = c;
     mStringLength = 1;
+    mMemoryCapacity = 1;
 }
 
 MyString::MyString(const char *str)
 {
     mStringLength = strlen(str);
+    mMemoryCapacity = mStringLength;
     mStringContent = new char[mStringLength];
 
     for (int i = 0; i != mStringLength; ++i)
@@ -23,6 +25,7 @@ MyString::MyString(const char *str)
 MyString::MyString(const MyString &str)
 {
     mStringLength = str.mStringLength;
+    mMemoryCapacity = mStringLength;
     mStringContent = new char[mStringLength];
 
     for (int i = 0; i < mStringLength; ++i)
@@ -41,6 +44,11 @@ int MyString::Length() const
     return mStringLength;
 }
 
+int MyString::Capacity() const
+{
+    return mMemoryCapacity;
+}
+
 void MyString::Print() const
 {
     for (int i = 0; i < mStringLength; ++i)
@@ -56,4 +64,64 @@ void MyString::Println() const
         std::cout << mStringContent[i];
     }
     std::cout << std::endl;
+}
+
+MyString &MyString::Assign(const MyString &str)
+{
+    if (str.mStringLength > mMemoryCapacity)
+    {
+        delete[] mStringContent;
+
+        mStringContent = new char[str.mStringLength];
+        mMemoryCapacity = str.mStringLength;
+    }
+
+    for (int i = 0; i < str.mStringLength; ++i)
+    {
+        mStringContent[i] = str.mStringContent[i];
+    }
+
+    mStringLength = str.mStringLength;
+
+    return *this;
+}
+
+MyString &MyString::Assign(const char *str)
+{
+    int length = strlen(str);
+
+    if (length > mMemoryCapacity)
+    {
+        delete[] mStringContent;
+
+        mStringContent = new char[length];
+        mMemoryCapacity = length;
+    }
+
+    for (int i = 0; i < length; ++i)
+    {
+        mStringContent[i] = str[i];
+    }
+
+    mStringLength = length;
+
+    return *this;
+}
+
+void MyString::Reserve(int size)
+{
+    if (size > mMemoryCapacity)
+    {
+        char *prev_string_content = mStringContent;
+
+        mStringContent = new char[size];
+        mMemoryCapacity = size;
+
+        for (int i = 0; i != mStringLength; ++i)
+        {
+            mStringContent[i] = prev_string_content[i];
+        }
+
+        delete[] prev_string_content;
+    }
 }
--- MyString.h
+++ MyString.h
@@ -6,13 +6,19 @@ class MyString {
 private:
     char *mStringContent;
     int mStringLength;
+    int mMemoryCapacity;
 
 public:
     MyString(char c);
     MyString(const char *str);
     MyString(const MyString &str);
 
+    MyString& Assign(const MyString &str);
+    MyString& Assign(const char* str);
+    void Reserve(int size);
+
     int Length() const;
+    int Capacity() const;
 
     void Print() const;
     void Println() const;
